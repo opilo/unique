455edd2d1df9d8b8ba19096459c08744af106f2d
Mohammad Reza <parsidev@users.noreply.github.com>
2015-04-04 18:32:26 +0430

    Update UniqueServiceProvider.php

--- src/Parsidev/Unique/UniqueServiceProvider.php
+++ src/Parsidev/Unique/UniqueServiceProvider.php
@@ -1,4 +1,6 @@
-<?php namespace Parsidev\Unique;
+<?php
+
+namespace Parsidev\Unique;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -6,16 +8,9 @@ class UniqueServiceProvider extends ServiceProvider {
 
     protected $defer = false;
 
-    public function boot() {
-        $this->publishes([
-            __DIR__ . '/../../config/pars_unique.php' => config_path('pars_unique.php'),
-        ]);
-    }
-
     public function register() {
         $this->app['unique'] = $this->app->share(function($app) {
-            $config = config('unique');
-            return new Unique($config);
+            return new Unique();
         });
     }
 
